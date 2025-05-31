5d8265f0caf5151f0b1a8294d88ab1550227e2f1
Anchal07777 <97102555+Anchal07777@users.noreply.github.com>
2025-05-31 16:16:42 +0530

    Time: 416 ms (5.02%), Space: 12.8 MB (91.03%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -6,7 +6,7 @@ public:
         while(k--){
             sort(nums.begin(),nums.end());
             nums[n-1] = floor(sqrt(nums[n-1]));
-            cout<<nums[0]<<" ";
+           
         }
         
         for(int i=0;i<n;i++){
