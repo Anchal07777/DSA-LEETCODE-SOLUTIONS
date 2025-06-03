adee877fbf6220654419d19b711a22340d771844
Anchal07777 <97102555+Anchal07777@users.noreply.github.com>
2025-06-03 16:41:30 +0530

    Time: 4 ms (63.37%), Space: 23.1 MB (92.39%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int returnToBoundaryCount(vector<int>& nums) {
+        int c = 0,sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(sum == 0)c++;
+        }
+        return c;
+    }
+};
